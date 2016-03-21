0541a92195a4960f432e745e0645da4da247ea20
Adrián Úbeda <adrianubedaportugues@gmail.com>
2016-03-21 10:55:35 +0100

    Ejercicio realizado

--- project/urls.py
+++ project/urls.py
@@ -7,4 +7,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(\d+)\+(\d+)$', 'calc.views.suma'),
+    url(r'^(\d+)\-(\d+)$', 'calc.views.resta'),
+    url(r'^(\d+)\/(\d+)$', 'calc.views.div'),
+    url(r'^(\d+)\*(\d+)$', 'calc.views.multi'),
+    url(r'.', 'calc.views.error')
 )
